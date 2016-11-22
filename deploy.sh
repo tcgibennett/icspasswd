e769528e0278aa75707d55c873f94fb5b8299da4
Thomas Bennett <tbennett@Thomass-MBP.attlocal.net>
2016-11-22 16:43:29 -0500

    completed 1.0 release - forgot stub.sh and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+mvn validate
+mvn test
+mvn clean compile assembly:single
+cp ./target/icspasswd-1.0-jar-with-dependencies.jar ./icspasswd.jar
+cat stub.sh icspasswd.jar > icspasswd && chmod 777 icspasswd
+~/Downloads/launch4j/launch4j ~/metaopsis/icspasswd.xml
