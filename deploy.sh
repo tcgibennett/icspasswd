f8b49ce1e808004e0858cf64225161e3236ecc12
tbennett@unicosolution.com <tbennett@unicosolution.com>
2018-05-24 19:16:30 -0400

    updating git repository from github to aws codecommit

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,6 @@ mvn clean compile assembly:single
 cp ./target/icspasswd-1.0-jar-with-dependencies.jar ./icspasswd.jar
 cat stub.sh icspasswd.jar > icspasswd && chmod 777 icspasswd
 ~/Downloads/launch4j/launch4j ~/metaopsis/icspasswd.xml
+
+cp ./icspasswd ~/IdeaProjects/icscmd
+cp ./icspasswd.exe ~/IdeaProjects/icscmd
--- src/main/java/com/metaopsis/icspasswd.java
+++ src/main/java/com/metaopsis/icspasswd.java
@@ -1,5 +1,14 @@
 package com.metaopsis;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 import com.metaopsis.crypto.Encryptor;
 
 /**
@@ -12,12 +21,40 @@ public class icspasswd {
         {
             help();
         } else {
-            System.out.println("Encrypted Password: " + Encryptor.encrypt(args[0]));
+        	try
+        	{
+        	    if (args[0].equals("-nv"))
+				System.out.println(Encryptor.encrypt(args[1]));
+			else
+        	    		System.out.println("Encrypted Password: " + Encryptor.encrypt(args[0]));
+        	} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidAlgorithmParameterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalBlockSizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
     }
 
     private static void help()
     {
-        System.out.println("usage: icspasswd <password>");
+        System.out.println("usage: icspasswd [-nv] <password>");
+        System.out.println("\t -nv: Suppress verbiage (i.e. Encrypted Password: )");
     }
 }
